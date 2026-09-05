d8c45c0ac555d65d1b7b85a1a79a7d42c4468e84
4oh4 <4oh4.git@gmail.com>
2026-09-05 18:20:31 +0100

    fix: dropout off for eval

--- model.py
+++ model.py
@@ -94,7 +94,7 @@ class GQA(nn.Module):
         v = v.contiguous()
         output = F.scaled_dot_product_attention(q, k, v,
                                                 attn_mask=mask,
-                                                dropout_p=self.dropout,
+                                                dropout_p=self.dropout if self.training else 0.0,
                                                 enable_gqa=True)
         output = output.transpose(1, 2).reshape(q_batch_size, q_seq_len, hidden_dim).contiguous()
         output = self.out_proj(output)
